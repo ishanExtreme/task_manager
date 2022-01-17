629c46c1e1b35ca644e6c9403a6f82914aa5fe9c
Extreme <extreme@pop-os.localdomain>
2022-01-17 18:41:49 +0530

    add and done functionality added

---
+++ solve_me.py
@@ -0,0 +1,137 @@
+from ast import arg
+
+
+class TasksCommand:
+    TASKS_FILE = "tasks.txt"
+    COMPLETED_TASKS_FILE = "completed.txt"
+
+    current_items = {}
+    completed_items = []
+
+    def read_current(self):
+        try:
+            file = open(self.TASKS_FILE, "r")
+            for line in file.readlines():
+                item = line[:-1].split(" ")
+                self.current_items[int(item[0])] = " ".join(item[1:])
+            file.close()
+        except Exception:
+            pass
+
+    def read_completed(self):
+        try:
+            file = open(self.COMPLETED_TASKS_FILE, "r")
+            self.completed_items = file.readlines()
+            file.close()
+        except Exception:
+            pass
+
+    def write_current(self):
+        with open(self.TASKS_FILE, "w+") as f:
+            f.truncate(0)
+            for key in sorted(self.current_items.keys()):
+                f.write(f"{key} {self.current_items[key]}\n")
+
+    def write_completed(self):
+        with open(self.COMPLETED_TASKS_FILE, "w+") as f:
+            f.truncate(0)
+            for item in self.completed_items:
+                f.write(f"{item}\n")
+
+    def run(self, command, args):
+        self.read_current()
+        self.read_completed()
+        if command == "add":
+            self.add(args)
+        elif command == "done":
+            self.done(args)
+        elif command == "delete":
+            self.delete(args)
+        elif command == "ls":
+            self.ls()
+        elif command == "report":
+            self.report()
+        elif command == "help":
+            self.help()
+
+    def help(self):
+        print(
+            """Usage :-
+$ python tasks.py add 2 hello world # Add a new item with priority 2 and text "hello world" to the list
+$ python tasks.py ls # Show incomplete priority list items sorted by priority in ascending order
+$ python tasks.py del PRIORITY_NUMBER # Delete the incomplete item with the given priority number
+$ python tasks.py done PRIORITY_NUMBER # Mark the incomplete item with the given PRIORITY_NUMBER as complete
+$ python tasks.py help # Show usage
+$ python tasks.py report # Statistics"""
+        )
+
+    def add(self, args):
+        """
+        Add task to list
+    
+        Task is added according to the priority and than it is saved in file
+    
+        Parameters:
+        args: list of arguments
+    
+        """
+        if(len(args) < 2):
+            print("Error: Missing tasks string. Nothing added!")
+        else:
+
+            inc_priority = int(args[0])
+            while(True):
+                temp = None
+                temp_arr = [key for key in self.current_items if key == inc_priority]
+                for key in temp_arr:
+                        self.current_items[inc_priority+1] = self.current_items[inc_priority]
+                        inc_priority+=1
+                if(temp == None):
+                    break
+
+            # self.current_items[int(item[0])] = " ".join(item[1:])
+            self.current_items[int(args[0])] = args[1]
+            self.write_current()
+            print(f'Added task: "{args[1]}" with priority {args[0]}')
+        
+
+    def done(self, args):
+        """
+        Mark a task as complete
+    
+        Use the done command to mark an item as completed by its index.
+    
+        Parameters:
+        args: list of arguments
+    
+        """
+
+        if(len(args) < 1):
+             print("Error: Missing NUMBER for marking tasks as done.")
+        else:
+            try:
+                index = int(args[0])
+                # delete from incomplete list
+                task_name = self.current_items.pop(index)
+                # reflect it to file
+                self.write_current()
+                # add name to complete list
+                self.completed_items.append(task_name)
+                # reflect to file
+                self.write_completed()
+
+                print("Marked item as done.")
+        
+            except:
+                print(f"Error: no incomplete item with priority {index} exists.")
+
+
+
+    def delete(self, args):
+        pass
+
+    def ls(self):
+        pass
+
+    def report(self):
+        pass
