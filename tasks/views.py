d4e2df5266d7daa7c88fe656a0d77c8e27ab2a68
Extreme <extreme@pop-os.localdomain>
2022-01-21 17:55:56 +0530

    added styling

--- tasks/views.py
+++ tasks/views.py
@@ -5,15 +5,24 @@ tasks = []
 
 
 def add_task(request):
+    """
+    Add a new task to the list
+    """
     task_value = request.GET.get("task")
     tasks.append(task_value)
     return HttpResponseRedirect("/tasks")
 
 
 def display_tasks(request):
+    """
+    Display All tasks
+    """
     return render(request, "tasks.html", {"tasks": tasks})
 
 
 def delete_task(request, index):
+    """
+    Delete task from the list. Takes index of the task as parameter
+    """
     tasks.pop(index - 1)
     return HttpResponseRedirect("/tasks")
