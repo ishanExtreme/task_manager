b84e589d61129883f4099acfd43f88059a8ed872
Extreme <extreme@pop-os.localdomain>
2022-01-21 17:25:33 +0530

    delete and add functionality added

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from tasks import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("add-task/", views.add_task),
+    path("tasks/", views.display_tasks),
+    path("delete-task/<int:index>/", views.delete_task)
     # Add all your views here
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,19 @@
-# Add all your views here
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
+
+
+def add_task(request):
+    task_value = request.GET.get("task")
+    tasks.append(task_value)
+    return HttpResponseRedirect("/tasks")
+
+
+def display_tasks(request):
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def delete_task(request, index):
+    tasks.pop(index - 1)
+    return HttpResponseRedirect("/tasks")
