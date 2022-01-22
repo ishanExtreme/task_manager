9f18841f85d03e3c048be46f6d0a7e0ed9a8f0a3
Extreme <extreme@pop-os.localdomain>
2022-01-22 16:08:13 +0530

    upto level-4 functionality added

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from tasks import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("add-task/", views.add_task),
+    path("tasks/", views.display_tasks),
+    path("delete-task/<int:index>/", views.delete_task),
+    path("complete_task/<int:index>/", views.mark_complete),
+    path("completed_tasks/", views.display_completed_tasks),
+    path("all_tasks/", views.display_all_tasks),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,53 @@
-# Add your Views Here
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
+complete = []
+
+
+def add_task(request):
+    """
+    Add a new task to the list
+    """
+    task_value = request.GET.get("task")
+    tasks.append(task_value)
+    return HttpResponseRedirect("/tasks")
+
+
+def display_tasks(request):
+    """
+    Display incomplete tasks
+    """
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def delete_task(request, index):
+    """
+    Delete task from the list. Takes index of the task as parameter
+    """
+    tasks.pop(index - 1)
+    return HttpResponseRedirect("/tasks")
+
+
+def mark_complete(request, index):
+    """
+    Marks task as complete by removing it from taks list and
+    adding it to complete list. Takes index of the task as parameter
+    """
+    index -= 1
+    complete.append(tasks.pop(index))
+    return HttpResponseRedirect("/tasks")
+
+
+def display_completed_tasks(request):
+    """
+    Displays completed tasks
+    """
+    return render(request, "completed.html", {"tasks": complete})
+
+
+def display_all_tasks(request):
+    """
+    Displays both pending and completed tasks
+    """
+    return render(request, "all_task.html", {"complete": complete, "pending": tasks})
