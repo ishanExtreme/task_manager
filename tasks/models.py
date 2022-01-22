86ba79b8b4a7785d95430bc9b0402697ad57236b
Extreme <extreme@pop-os.localdomain>
2022-01-22 16:28:56 +0530

    view add delete added

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,7 @@
-
 from django.db import models
 
 from django.contrib.auth.models import User
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
@@ -9,7 +9,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
-    user = models.ForeignKey(User , on_delete=models.CASCADE , null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
+from turtle import title
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from tasks.models import Task
 
-tasks = []
-complete = []
+# tasks = []
+# complete = []
 
 
 def add_task(request):
@@ -10,7 +12,7 @@ def add_task(request):
     Add a new task to the list
     """
     task_value = request.GET.get("task")
-    tasks.append(task_value)
+    Task(title=task_value).save()
     return HttpResponseRedirect("/tasks")
 
 
@@ -18,6 +20,11 @@ def display_tasks(request):
     """
     Display incomplete tasks
     """
+    search_term = request.GET.get("search")
+    tasks = Task.objects.filter(deleted=False)
+    if search_term:
+        tasks = tasks.filter(title__icontains=search_term)
+
     return render(request, "tasks.html", {"tasks": tasks})
 
 
@@ -25,7 +32,7 @@ def delete_task(request, index):
     """
     Delete task from the list. Takes index of the task as parameter
     """
-    tasks.pop(index - 1)
+    Task.objects.filter(id=index).update(deleted=True)
     return HttpResponseRedirect("/tasks")
 
 
