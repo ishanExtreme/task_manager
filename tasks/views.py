e75180ca74aefc841eed03d3082fed72a940ca5a
Extreme <extreme@pop-os.localdomain>
2022-01-22 16:40:35 +0530

    mark complete added

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,11 @@
-from turtle import title
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from tasks.models import Task
-
-# tasks = []
-# complete = []
 
 
 def add_task(request):
     """
-    Add a new task to the list
+    Add a new task to the model
     """
     task_value = request.GET.get("task")
     Task(title=task_value).save()
@@ -18,11 +14,13 @@ def add_task(request):
 
 def display_tasks(request):
     """
-    Display incomplete tasks
+    Display incomplete tasks and searches tasks
     """
     search_term = request.GET.get("search")
-    tasks = Task.objects.filter(deleted=False)
+    # query returns all the tasks which are not deleted and not marked complete
+    tasks = Task.objects.filter(deleted=False).filter(completed=False)
     if search_term:
+        # case insensitive filter to display search terms
         tasks = tasks.filter(title__icontains=search_term)
 
     return render(request, "tasks.html", {"tasks": tasks})
@@ -30,8 +28,9 @@ def display_tasks(request):
 
 def delete_task(request, index):
     """
-    Delete task from the list. Takes index of the task as parameter
+    Delete task from the list. Takes primary key of the task as parameter
     """
+    # soft delete the task
     Task.objects.filter(id=index).update(deleted=True)
     return HttpResponseRedirect("/tasks")
 
@@ -39,10 +38,9 @@ def delete_task(request, index):
 def mark_complete(request, index):
     """
     Marks task as complete by removing it from taks list and
-    adding it to complete list. Takes index of the task as parameter
+    adding it to complete list. Takes primary key of the task as parameter
     """
-    index -= 1
-    complete.append(tasks.pop(index))
+    Task.objects.filter(id=index).update(completed=True)
     return HttpResponseRedirect("/tasks")
 
 
@@ -50,11 +48,21 @@ def display_completed_tasks(request):
     """
     Displays completed tasks
     """
-    return render(request, "completed.html", {"tasks": complete})
+    search_term = request.GET.get("search")
+    # query returns all the tasks which are not deleted and marked complete
+    tasks = Task.objects.filter(deleted=False).filter(completed=True)
+    if search_term:
+        # case insensitive filter to display search terms
+        tasks = tasks.filter(title__icontains=search_term)
+    return render(request, "completed.html", {"tasks": tasks})
 
 
 def display_all_tasks(request):
     """
     Displays both pending and completed tasks
     """
+    # query returns all the tasks which are not deleted and not completed
+    tasks = Task.objects.filter(deleted=False).filter(completed=False)
+    # query returns all the tasks which are not deleted and marked complete
+    complete = Task.objects.filter(deleted=False).filter(completed=True)
     return render(request, "all_task.html", {"complete": complete, "pending": tasks})
