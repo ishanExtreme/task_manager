e058bca5f8a315ea14372c3b71fba453adae4456
Extreme <extreme@pop-os.localdomain>
2022-02-18 11:42:39 +0530

    settings changed to deployments

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hn4*^-v@9xjaxl&+-1u$hrl*d5wn^&j5_ir%18$-j2z@77u-g6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["extreme-task-manager.herokuapp.com"]
 
 
 # Application definition
