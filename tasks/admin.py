0f7db1a804cfe890917512510f18ae8b68abbfea
Extreme <extreme@pop-os.localdomain>
2022-04-25 22:44:10 +0530

    admin updated

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from tasks.models import Task, History, Schedule
+from tasks.models import Task, History, Schedule, Board, Stage
 
 admin.sites.site.register(Task)
 admin.sites.site.register(History)
 admin.sites.site.register(Schedule)
+admin.sites.site.register(Board)
+admin.sites.site.register(Stage)
